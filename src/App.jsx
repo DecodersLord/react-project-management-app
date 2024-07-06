17987877037a4552d38512c9039f4b3006615029
DecodersLord <PRIYANK.SEVAK1996@GMAIL.COM>
2024-07-05 23:39:27 -0400

    Cosmetic Changes to form

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import CreateProject from "./Components/CreateProject";
 function App() {
     return (
         <>
-            <h1 className="my-8 text-center text-5xl font-bold">
-                React Project Management
-            </h1>
-            <div className="flex flex-row">
-                <Sidebar />
-                <CreateProject />
+            <div className="font-arsenal">
+                <h1 className="my-8 text-center text-5xl font-bold">
+                    React Project Management
+                </h1>
+                <div className="flex flex-row">
+                    <Sidebar />
+                    <CreateProject />
+                </div>
             </div>
         </>
     );
--- src/Components/CreateProject.jsx
+++ src/Components/CreateProject.jsx
@@ -2,20 +2,41 @@ export default function CreateProject() {
     return (
         <form
             action=""
-            className="flex flex-col bg-gray-400 w-96 h-fit justify-center mx-auto mt-20 p-8"
+            className="flex flex-col h-fit w-9/12 justify-center ml-2 mr-auto mt-20 p-8 font-medium"
         >
             <label htmlFor="Title" className="mt-4">
-                Title
+                TITLE
             </label>
-            <input type="text" name="title" id="title" />
+            <input
+                type="text"
+                name="title"
+                id="title"
+                className="bg-zinc-200 focus:border-b-2 focus:outline-none focus:border-b-black"
+            />
             <label htmlFor="Description" className="mt-4">
-                Description
+                DESCRIPTION
             </label>
-            <input type="text" name="Description" id="Description" />
+            <textarea
+                type="text"
+                name="Description"
+                id="Description"
+                className="bg-zinc-200 focus:border-b-2 focus:outline-none focus:border-b-black"
+            />
             <label htmlFor="Title" className="mt-4">
-                Due Date
+                DUE DATE
             </label>
-            <input type="date" name="title" id="title" />
+            <input
+                type="date"
+                name="title"
+                id="title"
+                className="bg-zinc-200 focus:border-b-2 focus:outline-none focus:border-b-black"
+            />
+            <div className="flex flex-row space-x-4 my-4 justify-end text-lg">
+                <button className="font-bold">Cancel</button>
+                <button className="bg-black text-white font-bold rounded-lg px-4">
+                    Save
+                </button>
+            </div>
         </form>
     );
 }
