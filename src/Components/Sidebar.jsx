46897d6ef3a4f9b2a2abe0732c9d347c1ae5b8e4
DecodersLord <PRIYANK.SEVAK1996@GMAIL.COM>
2024-09-20 23:53:31 -0400

    code-review initial commit

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,10 +1,27 @@
-export default function Sidebar() {
+import { useState } from "react";
+
+export default function Sidebar({ projects, selectProject, createProject }) {
     return (
-        <aside className="bg-black w-64 text-white text-2xl rounded-lg h-screen left-0">
+        <aside className="bg-black w-64 text-white text-2xl rounded-r-lg h-screen">
             <h2 className="p-6">Your Project</h2>
-            <button className="bg-gray-800 m-6 rounded-md w-fit px-2 py-2 text-sm">
-                + Create new Project
-            </button>
+            <ul>
+                {projects?.map((project) => (
+                    <li key={project.id || project.title}>
+                        <button
+                            className="bg-gray-800 m-2 rounded-md w-fit px-2 py-2 text-sm"
+                            onClick={() => selectProject(project.id)}
+                        >
+                            {project.title}
+                        </button>
+                    </li>
+                ))}
+                <button
+                    className="bg-green-800 flex m-6 rounded-md w-fit px-2 py-2 text-sm"
+                    onClick={() => createProject(true)}
+                >
+                    + Create Project
+                </button>
+            </ul>
         </aside>
     );
 }
